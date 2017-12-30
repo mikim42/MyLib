250fce1f93b7b00a81ef0a053e7b6407facabffb
Mingyun KIM <mikim@e1z2r6p4.42.us.org>
2017-12-29 18:57:29 -0800

    Mylib: Fixed ft_printf %C %S

--- srcs/parse_spec.c
+++ srcs/parse_spec.c
@@ -6,7 +6,7 @@
 /*   By: mikim <mikim@student.42.us.org>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/07 03:25:42 by mikim             #+#    #+#             */
-/*   Updated: 2017/10/15 00:46:41 by mikim            ###   ########.fr       */
+/*   Updated: 2017/12/29 18:56:55 by mikim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,8 +50,6 @@ void	get_spec(const char *restrict fmt, t_pf_env *e)
 		spec_unsint(e, fmt[e->i]);
 	else if ((fmt[e->i] == 'c' || fmt[e->i] == 's') && e->mod != pf_l)
 		spec_char(e, fmt[e->i]);
-	else if (fmt[e->i] == 'C' || fmt[e->i] == 'S')
-		spec_char(e, fmt[e->i] + 32);
 	else if (((fmt[e->i] == 'c' || fmt[e->i] == 's') && e->mod == pf_l) ||
 			fmt[e->i] == 'C' || fmt[e->i] == 'S')
 		spec_wchar(e, fmt[e->i]);
